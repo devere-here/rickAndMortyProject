9e62ed6ca216ec6d5c868044f1b268a634f3d830
Steven DeVere <sdevere@xogrp.com>
2021-05-11 00:08:16 -0400

    Requests character data in CharacterPage

--- components/CharacterPage.tsx
+++ components/CharacterPage.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from '@apollo/client';
+import { Text } from "react-native";
 
 interface CharacterPageParams {
   id: string;
 }
 
+const CHARACTER_QUERY = gql`
+  query Character($id: ID!){
+    character(id: $id) {
+      id
+      name
+    }
+  }
+`
+
 export default function CharacterPage() {
   const { id } = useParams<CharacterPageParams>();
+  const { data, loading } = useQuery(CHARACTER_QUERY, {
+    variables: { id }
+  });
 
   return (
-    <div>
-      On the character page for character number {id}
-    </div>
+    <>
+      {!loading && <Text>On the character page for character {data?.character?.name}</Text>}
+    </>
   );
 }
